806e2857bd8886845c653963c030711546989f1a
Nick-Stoyanov <99138648+Nick-Stoyanov@users.noreply.github.com>
2024-12-04 14:03:19 -0500

    deploy 1.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        unoptimized: true, // Disable default image optimization
+    },
+    assetPrefix: isProd ? '/Cuisine-on-the-Rhode/' : '',
+    basePath: isProd ? '/Cuisine-on-the-Rhode' : '',
+    output: 'export'
+};
 
 export default nextConfig;
