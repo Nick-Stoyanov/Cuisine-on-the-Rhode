5181b488a6d88ec2b007fffea6e7f3ab2277a117
Nick-Stoyanov <99138648+Nick-Stoyanov@users.noreply.github.com>
2024-12-05 15:46:15 -0500

    fix and working on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-    reactStrictMode: true, // Enables React strict mode
+    reactStrictMode: true,
     images: {
-        unoptimized: true, // Disables default image optimization for GitHub Pages
+        unoptimized: false, // Let Vercel handle image optimization
     },
-    assetPrefix: isProd ? "/Cuisine-on-the-Rhode/" : "", // Prefix for assets in production
-    basePath: isProd ? "/Cuisine-on-the-Rhode" : "", // Base path for routing in production
-    output: "export", // Ensures a static export
+    output: undefined, // Remove 'export' to enable Vercel's dynamic builds
 };
 
 export default nextConfig;
